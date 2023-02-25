4a50736013aead9b0cda811d53151a9a28d4c2bd
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2023-02-25 06:08:07 +0100

    feat: add basic models for auction listing, bid and comment

--- auctions/models.py
+++ auctions/models.py
@@ -15,3 +15,20 @@ class User(AbstractUser):
 # 1) python manage.py makemigrations 
 # 2) python manage.py migrate 
 # to migrate those changes to your database
+
+class AuctionListing():
+    user_id = models.IntegerField()
+    title = models.CharField(max_length=150)
+    price = models.IntegerField()
+    #photo = models.URLField()
+
+class Bid():
+    user_id = models.IntegerField()
+    listing_id = models.IntegerField()
+    value = models.IntegerField()
+
+class Comment():
+    user_id = models.IntegerField()
+    listing_id = models.IntegerField()
+    body = models.CharField(max_length=500)
+    
