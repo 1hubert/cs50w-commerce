852bf31e670101af2cf5b72612028e030f1e64a4
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2023-02-25 05:57:53 +0100

    feat: register user model for admin use + create superuser

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+# You will also need to add additional models to this file to represent details about (3 required, 1 optional):
+# auction listings
+# bids
+# comments
+# auction categories (model is optional, category functionality is required)
+
+# Remember that each time you change anything in auctions/models.py, you’ll need to first run:
+# 1) python manage.py makemigrations 
+# 2) python manage.py migrate 
+# to migrate those changes to your database
